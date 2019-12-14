24cfa49683ffe46c2043a9f2d79a8a4c4231196e
314ronin <pownmymail@gmail.com>
2019-12-15 02:49:25 +0300

    index page created

---
+++ src/main/java/val/example/demo/controller/IndexController.java
@@ -0,0 +1,20 @@
+package val.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    String index(Model model){
+
+        model.addAttribute("number", "12345");
+
+        return "index";
+    }
+}
