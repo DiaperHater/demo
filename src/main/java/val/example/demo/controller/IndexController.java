c19e1b47abc0b6387b06f74e192c3693a0429715
314ronin <pownmymail@gmail.com>
2019-12-16 01:04:14 +0300

    display authors on index page

--- src/main/java/val/example/demo/controller/IndexController.java
+++ src/main/java/val/example/demo/controller/IndexController.java
@@ -1,16 +1,28 @@
 package val.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import val.example.demo.model.Author;
+import val.example.demo.repo.AuthorRepo;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    private AuthorRepo authorRepo;
+
+    @Autowired
+    public IndexController(AuthorRepo authorRepo) {
+        this.authorRepo = authorRepo;
+    }
+
     @RequestMapping("/index")
     String index(Model model){
 
-        model.addAttribute("number", "12345");
+        model.addAttribute("authors", authorRepo.findAll());
 
         return "index";
     }
